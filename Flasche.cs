4f6e33c37f1686a9daac7845661f6cbe36aa9374
Yusuf Bastürk <ybastuerk@schueler.hakzell.at>
2022-04-20 08:42:51 +0200

    Projektdateien hinzufügen.

---
+++ Flasche.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getränk
+{
+    internal class Flasche
+    {
+        public double Durchmesser { get; set; }
+
+        public double Höhe { get; set; }
+
+        public double Volumen {
+            get
+            {
+              return  Math.Pow(Durchmesser / 2, 2) * Math.PI * Höhe;
+            }
+
+
+
+}
+            
+
+        public Flasche(double durchmesser, double höhe)
+        {
+            this.Durchmesser = durchmesser;
+
+            this.Höhe = höhe
+
+
+ }       }
+}
